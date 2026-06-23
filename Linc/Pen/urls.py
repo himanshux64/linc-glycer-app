20f3c187787c879cb958a522c982123897ea6aca
Himanshu Kumar <himanshukumar51667@gmail.com>
2026-06-23 16:54:41 +0530

    Add migrations for Penvarity model: introduce and modify quantity field, remove it, and update views and templates for pen details

--- Linc/Pen/urls.py
+++ Linc/Pen/urls.py
@@ -4,5 +4,6 @@ from .import views
 
 
 urlpatterns = [
-    path('',views.info,name='info')
+    path('',views.info,name='info'),
+    path('<int:pen_id>/',views.pen_details,name='pen_deatils'),
 ]
--- Linc/Pen/views.py
+++ Linc/Pen/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from .models import Penvarity
+from django.shortcuts import get_list_or_404,get_object_or_404
 # Create your views here.
 
 
 def info(request):
     pens=Penvarity.objects.all() # fetch all data in database
     return render(request,'Pen/info.html',{'pens':pens})
+
+
+def pen_details(request,pen_id):
+    pen = get_object_or_404(Penvarity,pk=pen_id)
+    return render(request,'Pen/404.html',{'pen':pen})
